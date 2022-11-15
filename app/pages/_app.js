c3f5dc9297b6d09fd015eb93763d5ce3c197705e
clement <clement.conchezboueytou@edu.ece.fr>
2022-11-15 13:46:13 +0100

    feat: lab 6 save

---
+++ app/pages/_app.js
@@ -0,0 +1,10 @@
+import '../styles/globals.css'
+import {UserContextProvider} from '../components/UserContext'
+
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <UserContextProvider>
+      <Component {...pageProps} />
+    </UserContextProvider>
+  )
+}
