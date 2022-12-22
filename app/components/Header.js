984b5d20d242f32a3e07cc22fd0761d2f5f08baf
clement <clement.conchezboueytou@edu.ece.fr>
2022-12-22 15:25:18 +0100

    feat: header

---
+++ app/components/Header.js
@@ -0,0 +1,12 @@
+
+
+export default function Header(){
+  return (
+    <header className="flex bg-slate-200 px-10 py-2">
+      <div>
+        <input type="text" />
+        
+      </div>
+    </header>
+  )
+}
--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -2,14 +2,15 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
+import Header from '../components/header'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <h1 className="text-6xl font-bold underline text-indigo-500 ">
-      Hello world!
-    </h1>
+    <div>
+      <Header></Header>
+    </div>
     
   )
 }
