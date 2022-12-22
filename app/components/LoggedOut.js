cdcdcf3022b2e40a5878a3b4ac4775be60b03cc8
clement <clement.conchezboueytou@edu.ece.fr>
2022-12-22 15:50:18 +0100

    fix: Connexion Components

---
+++ app/components/LoggedOut.js
@@ -0,0 +1,20 @@
+import { useContext } from 'react'
+import UserContext from './UserContext'
+
+export default function LoggedOut(){
+    const {login} = useContext(UserContext)
+    const onClickLogin = async (e) => {
+      const response = await fetch('/api/profile')
+      const user = await response.json()
+      login(user)
+    }
+    return (
+      <button
+        className="flex gap-2 [&_svg]:h-6 [&_svg]:w-6"
+        onClick={onClickLogin}
+      >
+        
+        Log-in
+      </button>
+    )
+  }
