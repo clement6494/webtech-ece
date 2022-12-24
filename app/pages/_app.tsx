892369e350d511965d375cb9fd1c0c353e71ee5f
clement <clement.conchezboueytou@edu.ece.fr>
2022-12-24 04:01:48 +0100

    feat: supabase auth ui

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -1,6 +1,20 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useState } from 'react'
+import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs'
+import { SessionContextProvider } from '@supabase/auth-helpers-react'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+function App ({ Component, pageProps }: AppProps) {
+  const [supabase] = useState(() => createBrowserSupabaseClient())
+
+  return (
+    <SessionContextProvider supabaseClient={supabase} initialSession={pageProps.initialSession}>
+      
+      
+        <Component {...pageProps} />
+      
+    </SessionContextProvider>
+  )
 }
+export default App
--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -3,14 +3,31 @@ import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
 import Header from '../components/header'
+import { Auth, ThemeSupa } from '@supabase/auth-ui-react'
+import { useSession, useSupabaseClient } from '@supabase/auth-helpers-react'
+import Account from '../components/Account'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+
+
+const Home = () => {
+  const session = useSession()
+  const supabase = useSupabaseClient()
+
   return (
     <div>
       <Header></Header>
+
+      <div className="container" style={{ padding: '50px 0 100px 0' }}>
+        {!session ? (
+          <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} theme="dark" />
+        ) : (
+          <Account session={session} />
+        )}
+      </div>
     </div>
-    
   )
 }
+
+export default Home
