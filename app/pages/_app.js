b52165f8038326d0c84f63c04d6135b218df828c
clement <clement.conchezboueytou@edu.ece.fr>
2022-12-26 03:10:49 +0100

    fix:

--- app/pages/_app.js
+++ app/pages/_app.js
@@ -6,15 +6,13 @@ import '../styles/globals.css'
 function MyApp({ Component, pageProps }) {
   const [user, setUser] = useState(null);
   useEffect(() => {
-    const { data: authListener } = supabase.auth.onAuthStateChange(
-      async () => checkUser()
-    )
+    const { data: authListener } = supabase.auth.onAuthStateChange(checkUser)
     checkUser()
     return () => {
       authListener?.unsubscribe()
     };
   }, [])
-  async function checkUser() {
+  function checkUser() {
     const user = supabase.auth.user()
     setUser(user)
   }
@@ -22,20 +20,24 @@ function MyApp({ Component, pageProps }) {
   <div>
     <nav className="p-6 border-b border-gray-300">
       <Link href="/">
-        <span className="mr-6 cursor-pointer">Home</span>
+        <span className="m-6">Home</span>
       </Link>
       {
         user && (
-          <Link href="/my-posts">
-            <span className="mr-6 cursor-pointer">My Posts</span>
-          </Link>,
           <Link href="/create-post">
-            <span className="mr-6 cursor-pointer">Create Post</span>
+            <span className="m-6">Create Post</span>
+          </Link>
+        )
+      }
+      {
+        user && (
+          <Link href="/my-posts">
+            <span className="m-6">My Posts</span>
           </Link>
         )
       }
       <Link href="/profile">
-        <span className="mr-6 cursor-pointer">Profile</span>
+        <span className="m-6">Profile</span>
       </Link>
     </nav>
     <div className="py-8 px-16">
