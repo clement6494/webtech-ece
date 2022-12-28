ea8311b836040695e7550df4470e8d97b26d86b0
clement <clement.conchezboueytou@edu.ece.fr>
2022-12-28 20:58:57 +0100

    fix: bugs

--- app/pages/api/supabase.js
+++ app/pages/api/supabase.js
@@ -5,3 +5,9 @@ export const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 )
 
+export const { data } = await supabaseAdmin.from('images').select('*').order('id')
+return {
+  props: {
+    images: data,
+  },
+}
--- app/pages/index.js
+++ app/pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Link from 'next/link'
 import Layout from '../components/Layout.js'
 
+
 //sections
 import Section1 from '../components/section1.js'
 
