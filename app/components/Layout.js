2b7941bc2d6b96b559846b7589e8820387ef32f8
clement <clement.conchezboueytou@edu.ece.fr>
2022-12-28 16:29:04 +0100

    fix: layout

--- app/components/Layout.js
+++ app/components/Layout.js
@@ -1,17 +1,16 @@
 import Header from '../components/Header.js'
 import Footer from '../components/Footer.js'
 
-export default function Layout({
-    children
-  }){
+export default function Layout({children})
+{
     return (
-      <div>
+      <>
         <Head> <title> webtech project</title></Head>
-        <Header />
+        <Header></Header>
         <main className="py-10 min-h-screen max-w-full md:max-w-2xl md:mx-auto">
         {children}
         </main>
-        <Footer />
-      </div>
+        <Footer></Footer>
+      </>
     )
   }
--- app/pages/index.js
+++ app/pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import Format from '../layout/format';
+import Format from '../components/Layout';
 
 // compoenents
 import Section1 from '../components/section1';
@@ -11,49 +11,18 @@ import Section4 from '../components/section4';
 import { useState, useEffect } from 'react'
 import Link from 'next/link'
 import { supabase } from '../api'
+import Layout from '../components/Layout';
 
 
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
 
-  <Format>
+  <Layout>
       <Section1></Section1>
       <Section2></Section2>
       <Section3></Section3>
       <Section4></Section4>
-   </Format>
+   </Layout>
    
 
 }
-
-/**      const [posts, setPosts] = useState([])
-  const [loading, setLoading] = useState(true)
-  useEffect(() => {
-    fetchPosts()
-  }, [])
-  async function fetchPosts() {
-    const { data, error } = await supabase
-      .from('posts')
-      .select()
-    setPosts(data)
-    setLoading(false)
-  }
-  if (loading) return <p className="text-2xl">Loading ...</p>
-  if (!posts) return <p className="text-2xl">No posts.</p>
-  return (
-    <div>
-      <Head></Head>
-      <h1 className="text-3xl font-semibold tracking-wide mt-6 mb-2">Posts</h1>
-      {
-        posts.map(post => (
-          <Link key={post.id} href={`/posts/${post.id}`}>
-            <div className="cursor-pointer border-b border-gray-300	mt-8 pb-4">
-              <h2 className="text-xl font-semibold">{post.title}</h2>
-              <p className="text-gray-500 mt-2">Author: {post.user_email}</p>
-            </div>
-          </Link>)
-        )
-      }
-    </div>
-  ) */
