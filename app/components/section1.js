620c206a8b3398b3bec83a0157f88f31f0512196
clement <clement.conchezboueytou@edu.ece.fr>
2022-12-28 18:37:22 +0100

    fix: path

--- app/components/section1.js
+++ app/components/section1.js
@@ -37,7 +37,6 @@ export default function Section1() {
     </section>     
   )
             
-
 }
 
 function Post( { data } ){
@@ -45,15 +44,15 @@ function Post( { data } ){
   return (
       <div className="item">
           <div className="images">
-              <Link href={`/posts/${id}`}><Image src={img || "/"} className="rounded" width={500} height={350} /></Link>
+              <Link href={`/articles/${id}`}><Image src={img || "/"} className="rounded" width={500} height={350} /></Link>
           </div>
           <div className="info flex justify-center flex-col py-4">
               <div className="cat">
-                  <Link href={`/posts/${id}`} className="text-orange-600 hover:text-orange-800">{category || "Unknown"}</Link>
-                  <Link href={`/posts/${id}`} className="text-gray-800 hover:text-gray-600">- {published || "Unknown"}</Link>
+                  <Link href={`/articles/${id}`} className="text-orange-600 hover:text-orange-800">{category || "Unknown"}</Link>
+                  <Link href={`/articles/${id}`} className="text-gray-800 hover:text-gray-600">- {published || "Unknown"}</Link>
               </div>
               <div className="title">
-                  <Link href={`/posts/${id}`} className="text-xl font-bold text-gray-800 hover:text-gray-600">{title || "Title"}</Link>
+                  <Link href={`/articles/${id}`} className="text-xl font-bold text-gray-800 hover:text-gray-600">{title || "Title"}</Link>
               </div>
               <p className="text-gray-500 py-3">
                   Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind 
