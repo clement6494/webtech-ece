b5cb70fda899a665b4869df4ff93636df0ced285
clement <clement.conchezboueytou@edu.ece.fr>
2022-12-28 18:00:33 +0100

    fix remove <a> in <link> for next13

--- app/components/_child/author.js
+++ app/components/_child/author.js
@@ -8,7 +8,7 @@ export default function author({ name, img, designation }) {
     <div className="author flex py-5">
         <Image src={img || ""} width={60} height={60} className="rounded-full"></Image>        
         <div className="flex flex-col justify-center px-4">
-            <Link href={"/"}><a className="text-md font-bold text-gray-800 hover:text-gray-600">{name || "No Name"}</a></Link>
+            <Link href={"/"} className="text-md font-bold text-gray-800 hover:text-gray-600">{name || "No Name"}</Link>
             <span className="text-sm text-gray-500">{designation || ""}</span>
         </div>
     </div>
