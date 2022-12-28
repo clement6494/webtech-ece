24aab1e207bbc7b732ad51256f83d662d6530f33
clement <clement.conchezboueytou@edu.ece.fr>
2022-12-28 22:35:38 +0100

    fix: post

--- app/components/post.js
+++ app/components/post.js
@@ -1,62 +1,47 @@
 import Image from "next/image"
 import Link from "next/link"
-import Author from "./_child/author"
-import fetcher from '../lib/fetcher'
-import Spinner from "./_child/spinner"
-import Error from "./_child/error"
 
-
-
-export default function Section1() {
-
-  const { data, isLoading, isError } = fetcher('api/posts')
-    
-    if(isLoading) return <Spinner></Spinner>;
-    if(isError) return <Error></Error>
-
-
-  return (
-
-    <section className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-      <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-
-            <h1 className="font-bold text-4xl pb-12 text-center">Latest articles</h1>
-
-            <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-14">
-            {
-                data.map((value, index) => (
-                    <Post data={value} key={index}></Post>
-                ))
-            }
-            </div>
-      </div>
-    </section>     
-  )
-            
+export async function getStaticProps() {
+const { data } = await supabaseAdmin.from('images').select('*').order('id')
+  return {
+    props: {
+      images: data,
+    },
+  }
 }
 
-function Post( { data } ){
-  const { id, title, category, img, published, author } = data;
+/** failed to read correctly data correctly from database so we used const */
+
+
+
+export default function  Post( { image } ){
+  const id=1 ;
+  const src='https://media.discordapp.net/attachments/1054536874120724561/1054552053348843530/CLEMENT_Paris_dieselpunk_neoclassical_architechture_72276e7c-b899-479d-80b0-7c6501fed911.png?width=696&height=696';
+  const category = 'architecture' ;
+  const date = '12/20/2022';
+  const author = 'clement conchez';
+  const title = 'The futur of urbanism in Paris';
+  const article = 'thats a long story';
   return (
-      <div className="item">
+      <Link href={`/articles/${id}`}>
           <div className="images">
-              <Link href={`/articles/${id}`}><Image src={img || "/"} className="rounded" width={500} height={350} /></Link>
+              <Image src={src || "/"} className="rounded" width={500} height={350} />
           </div>
+
           <div className="info flex justify-center flex-col py-4">
               <div className="cat">
-                  <Link href={`/articles/${id}`} className="text-orange-600 hover:text-orange-800">{category || "Unknown"}</Link>
-                  <Link href={`/articles/${id}`} className="text-gray-800 hover:text-gray-600">- {published || "Unknown"}</Link>
+                  <div className="text-orange-600 hover:text-orange-800">{category || "Unknown"}</div>
+                  <div className="text-gray-800 hover:text-gray-600">- {date || "Unknown"}</div>
               </div>
               <div className="title">
-                  <Link href={`/articles/${id}`} className="text-xl font-bold text-gray-800 hover:text-gray-600">{title || "Title"}</Link>
+                  <div className="text-xl font-bold text-gray-800 hover:text-gray-600">{title || "Title"}</div>
               </div>
               <p className="text-gray-500 py-3">
-                  Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind 
-                  text by the name of Lorem Ipsum decided to leave for the far World of Grammar.
+              <div className="text-xl text-gray-700 hover:text-gray-800">{article || "Title"}</div>
               </p>
-              { author ? <Author {...author}></Author> : <></>}
+              { author ? <p {...author}></p> : <></>}
           </div>
-      </div>
+          </Link>
   )
 }
 
--- app/pages/articles/[id].js
+++ app/pages/articles/[id].js
@@ -1,53 +1,43 @@
-import { useEffect, useState } from 'react'
 import Head from 'next/head'
 import Link from 'next/link'
 import Layout from '../../components/Layout.js'
 import { supabase } from '../api/supabase'
 
-export default function Article({
-  article
+export default function Articles({
+  articles
 }) {
   return (
     <Layout>
       <Head>
-        <title>WebTech - {article.title}</title>
+        <title>WebTech - articles</title>
         <meta name="description" content="WebTech articles page" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <h1 className='wt-title'>
-        {article.title}
+        Web technologies articles
       </h1>
       <p className="italic my-5">This page fetches data at build time, excellent for SEO.</p>
-      <p>
-        {article.message}
-      </p>
+      <ul>
+        {articles.map( article => (
+          <li key={article.slug} className="my-5">
+            <h2 className="font-bold mb-1"><Link href={`/articles/${article.slug}`}>{article.title}</Link></h2>
+            <p>{article.message}</p>
+          </li>
+        ))}
+      </ul>
     </Layout>
   )
 }
 
 export async function getStaticProps(ctx) {
-  let article = {}
-  let { data, error, status } = await supabase
-    .from('articles')
-    .select(`id, slug, message, title`)
-    .eq('slug', ctx.params.slug)
-    .single()
-  if (!error) article = data // handle errors
-  return {
-    props: {
-      article: article
-    }
-  };
-}
-
-export async function getStaticPaths(ctx) {
   let articles = []
   let { data, error, status } = await supabase
     .from('articles')
-    .select(`slug`)
+    .select(`id, slug, message, title`)
   if (!error) articles = data // handle errors
   return {
-    paths: articles.map( article => `/articles/${article.slug}`),
-    fallback: false
+    props: {
+      articles: articles
+    }
   };
 }
